b9d03f33a73468e9a6dbcb75f68b08c355321ead
Qgameplayer <quintentijl24@gmail.com>
2024-01-16 19:02:37 +0100

    movement + springen (niet af)

---
+++ Informatica2D PO/Assets/Scripts/BoboScript.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoboScript : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpHeight;
+
+    [SerializeField] private KeyCode left;
+    [SerializeField] private KeyCode right;
+    [SerializeField] private KeyCode jump;
+
+    private Rigidbody2D rb;
+
+    [SerializeField] private Transform groundCheckPoint;
+
+    [SerializeField] private bool isOnGround;
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private LayerMask groundCheckLayerMask;
+
+    [SerializeField] private GameObject bibi;
+    [SerializeField] private bool isOnPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        MovementManager();
+    }
+
+    private void MovementManager()
+    {
+
+        isOnGround = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, groundCheckLayerMask);
+        
+
+        if (Input.GetKey(left))
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+        }
+        else if (Input.GetKey(right))
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if (Input.GetKey(jump) && isOnGround)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+
+    }
+
+}
