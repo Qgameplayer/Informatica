d099162fad0c7830fbeb2bccf32a93994ab83afe
Qgameplayer <quintentijl24@gmail.com>
2024-01-31 07:41:21 +0100

    bobo oppakken en droppen
    bobo kan soort van blokje oppakken en neergooien. het werkt allemaal net wel net niet dus daar moet nog veel werk in geslopt worden.

--- Informatica2D PO/Assets/Scripts/Bobo/BoboScript.cs
+++ Informatica2D PO/Assets/Scripts/Bobo/BoboScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoboScript : MonoBehaviour
 {
@@ -19,14 +20,16 @@ public class BoboScript : MonoBehaviour
 
     internal Rigidbody2D rb;
 
-    private Canvas canvas;
-
 
     [SerializeField] private float pickupRange = 2f;
     [SerializeField] private LayerMask pickupLayer;
     private GameObject heldBlock;
-    private GameObject inventory;
+    [SerializeField] private GameObject inventory;
 
+    private float lastInventoryInputTime;
+    private int currentSlot = 0;
+
+    public Image[] inventorySlots;
 
     // Start is called before the first frame update
     void Start()
@@ -36,14 +39,15 @@ public class BoboScript : MonoBehaviour
         logic = GameObject.FindWithTag("Logic");
         logicScript = logic.GetComponent<LogicScript>();
 
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         HandlePickup();
-
+        HandleInventory();
+        HandleDrop();
     }
 
     void HandlePickup()
@@ -60,12 +64,68 @@ public class BoboScript : MonoBehaviour
                 heldBlock.SetActive(false);
             }
 
-            else if (hit2.collider != null) 
+            else if (hit2.collider != null)
             {
                 heldBlock = hit2.collider.gameObject;
                 heldBlock.SetActive(false);
             }
         }
 
+    }
+
+    private void HandleInventory()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            inventory.SetActive(!inventory.activeSelf);
+
+            if (inventory.activeSelf)
+            {
+                lastInventoryInputTime = Time.time;
+            }
+        }
+
+        if (inventory.activeSelf && Time.time - lastInventoryInputTime > 5f)
+        {
+            Debug.Log("test");
+            inventory.SetActive(false);
+        }
+
+        if (inventory.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                currentSlot = 0;
+                Debug.Log(currentSlot);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                currentSlot = 1;
+                Debug.Log(currentSlot);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                currentSlot = 2;
+                Debug.Log(currentSlot);
+            }
+        }
+        UpdateInventoryUI();
+    }
+
+    private void HandleDrop()
+    {
+        Debug.Log(inventory.activeSelf);
+        if (Input.GetKeyDown(KeyCode.I) && inventory.activeSelf && heldBlock != null && currentSlot < 3)
+        {
+            // Drop the block from the current inventory slot
+            heldBlock.transform.position = transform.position + Vector3.right * transform.localScale.x;
+            heldBlock.SetActive(true);
+            heldBlock = null;
+        }
+    }
+
+    private void UpdateInventoryUI()
+    {
+        
     }
 }
