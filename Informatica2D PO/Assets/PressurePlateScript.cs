c3bca147c71a3bf0592c055408392119440675a6
bessieboy <126597@edu.maaswaalcollege.nl>
2024-02-02 18:06:59 +0100

    pressure plate gemaakt
    pressure plate gemaakt die naar beneden gaat, stopt bij vloerhoogte, en weer terug omhoog gaat. nog een kleine error bij als deze helemaal beneden is

---
+++ Informatica2D PO/Assets/PressurePlateScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlateScript : MonoBehaviour
+{
+    public Vector3 originalPos; 
+    bool moveBack = false;
+
+
+    private void Start () { 
+    originalPos = transform.position;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.name == "Bobo")
+        {
+            collision.transform.parent = transform;
+            GetComponent<SpriteRenderer>().color = Color.red;
+        }
+
+        
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.transform.name == "Bobo")
+        {
+        
+            transform.Translate(0, -0.01f, 0);
+            
+            
+            moveBack = false;
+        }
+
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.name == "Bobo")
+        {
+            collision.transform.parent = null;
+            moveBack = true;
+            GetComponent<SpriteRenderer>().color = Color.white;
+        }
+    }
+    private void Update()
+    {
+        if (moveBack)
+        {
+            if (transform.position.y < originalPos.y) {
+                transform.Translate(0, 0.01f, 0);
+            }
+            else
+            {
+                moveBack = false;
+            }
+        }
+    }
+}
