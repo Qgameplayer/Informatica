bfdd268db7f293fadcbc31a0c8cc87611f457dc8
Thijn <enigma4allofu@gmail.com>
2024-03-04 17:37:40 +0100

    Bobo bibi visual en animatie

---
+++ Informatica2D PO/Assets/PreFab/BOBO_ANIM.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOBO_ANIM : MonoBehaviour
+{
+
+	private Animator anim;
+
+	void Start(){
+		anim = GetComponent<Animator>();
+	}
+
+	void Update(){
+
+		if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)){
+
+			anim.SetBool("isRunning", true);
+
+		} else {
+			anim.SetBool("isRunning", false);
+		}
+	}
+
+}
