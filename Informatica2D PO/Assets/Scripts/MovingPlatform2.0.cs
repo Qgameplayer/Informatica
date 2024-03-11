5afdab82c0d86f05ac17d07533884bc85dfcc074
bessieboy <126597@edu.maaswaalcollege.nl>
2024-03-11 20:15:02 +0100

    x

--- Informatica2D PO/Assets/Scripts/MovingPlatform2.0.cs
+++ Informatica2D PO/Assets/Scripts/MovingPlatform2.0.cs
@@ -13,37 +13,36 @@ public class MovingPlatform2 : MonoBehaviour
     private void Update()
     {
         Vector2 target = currentTarget();
-        platform.position = Vector2.Lerp(platform.position, target, speed * Time.deltaTime);
+        platform.position = Vector2.MoveTowards(platform.position, target, speed * Time.deltaTime);
 
         float distance = (target - (Vector2)platform.position).magnitude;
 
         if (distance <= 0.1f)
         {
             direction *= -1;
-            }
+        }
     }
 
     Vector2 currentTarget()
     {
-        if(direction == 1)
+        if (direction == 1)
         {
             return startPoint.position;
         }
-        else 
+        else
         {
             return endPoint.position;
         }
     }
-        
 
 
-    private void OnDrawGizmos() 
+
+    private void OnDrawGizmos()
     {
-        if (platform != null && startPoint!= null && endPoint!= null) 
+        if (platform != null && startPoint != null && endPoint != null)
         {
             Gizmos.DrawLine(platform.transform.position, startPoint.position);
             Gizmos.DrawLine(platform.transform.position, endPoint.position);
         }
     }
-    
 }
